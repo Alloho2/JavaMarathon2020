14384ac234ba28c9d6cf7060be0c86af526c4db2
Andrey Baraboshin <alloho2@gmail.com>
2020-09-23 14:28:08 +0300

    Day3 solution

--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,24 @@
 package day3;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+            try {
+                int a = scanner.nextInt();
+                int b = scanner.nextInt();
+                if (b == 0) {
+                    System.out.println("Деление на 0");
+                    continue;
+                }
+                System.out.println(a * 1.0 / b);
+            } catch (InputMismatchException e) {
+                System.out.println("Ошибка ввода");
+                break;
+            }
+        }
     }
 }
--- src/main/java/day3/Task3var2.java
+++ src/main/java/day3/Task3var2.java
@@ -4,16 +4,24 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Task2 {
+//Решение через исключение
+
+public class Task3var2 {
     public static void main(String[] args) {
         try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
-            while (true) {
+            for (int i = 0; i < 5; i++) {
                 int a = Integer.parseInt(bufferedReader.readLine());
                 int b = Integer.parseInt(bufferedReader.readLine());
-                if (b == 0) {
-                    break;
+
+                try {
+                    double result = (a * 1.0 / b);
+                    if (result == Double.POSITIVE_INFINITY || result == Double.NEGATIVE_INFINITY) {
+                        throw new ArithmeticException();
+                    }
+                    System.out.println(result);
+                } catch (ArithmeticException e) {
+                    System.out.println("Деление на 0");
                 }
-                System.out.println(a * 1.0 / b);
             }
         } catch (NumberFormatException e) {
             System.out.println("Ошибка ввода");
