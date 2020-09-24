42d24097514a65fe4f429a68c8fbaf6318980bed
Andrey Baraboshin <alloho2@gmail.com>
2020-09-24 19:21:51 +0300

    Day4 Task3 Update

--- src/main/java/day4/Task3.java
+++ src/main/java/day4/Task3.java
@@ -6,26 +6,21 @@ public class Task3 {
     public static void main(String[] args) {
         Random random = new Random();
         int[][] number = new int[12][8];
+        int countTemp = 0;
+        int countTemp2;
+        int indexMaxLine = 0;
 
         for (int i = 0; i < number.length; i++) {
+            countTemp2 = 0;
             for (int j = 0; j < number[0].length; j++) {
                 number[i][j] = random.nextInt(50);
+                countTemp2 += number[i][j];
+            }
+            if (countTemp <= countTemp2) {
+                countTemp = countTemp2;
+                indexMaxLine = i;
             }
         }
-        int count = 0;
-        int count2;
-        int str = 0;
-
-        for (int i = 0; i < number.length; i++) {
-            count2 = 0;
-            for (int j = 0; j < number[0].length; j++) {
-                count2 += number[i][j];
-            }
-            if (count <= count2) {
-                count = count2;
-                str = i;
-            }
-        }
-        System.out.println("Ответ: " + str);
+        System.out.println("Ответ: " + indexMaxLine);
     }
 }
