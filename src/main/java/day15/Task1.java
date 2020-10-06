5b82026c941befaf320f3b80e490e0b42dcc7ef2
Andrey Baraboshin <alloho2@gmail.com>
2020-10-06 13:29:16 +0300

    Day15 solution update1

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,6 +1,12 @@
 package day15;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Task1 {
     public static void main(String[] args) {
@@ -12,16 +18,25 @@ public class Task1 {
         if (!file.getAbsolutePath().endsWith(".csv")) {
             System.out.println("Формат файла не верный");
         } else {
-            try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-                 FileOutputStream fos = new FileOutputStream("src/main/resources/missing_shoes.txt")) {
-                String st;
-
-                while ((st = bufferedReader.readLine()) != null) {
-                    int count = Integer.parseInt(st.substring(st.lastIndexOf(";") + 1));
-                    if (count == 0) {
-                        fos.write((st.replaceAll(";", ", ") + System.lineSeparator()).getBytes());
-                    }
-                }
+//            try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+//                 FileOutputStream fos = new FileOutputStream("src/main/resources/missing_shoes.txt")) {
+//                String st;
+//
+//                while ((st = bufferedReader.readLine()) != null) {
+//                    int count = Integer.parseInt(st.substring(st.lastIndexOf(";") + 1));
+//                    if (count == 0) {
+//                        fos.write((st.replaceAll(";", ", ") + System.lineSeparator()).getBytes());
+//                    }
+//                }
+//            } catch (NumberFormatException | IOException e) {
+//                System.out.println("Некорректный входной файл");
+//            }
+            try (Stream<String> stream = Files.lines(Paths.get(String.valueOf(file)))) {
+                Path path = Paths.get("src/main/resources/missing_shoes.txt");
+                List<String> result = stream.filter(a -> a.endsWith(";0"))
+                        .map(a -> a.replaceAll(";", ", "))
+                        .collect(Collectors.toList());
+                Files.write(path, result);
             } catch (NumberFormatException | IOException e) {
                 System.out.println("Некорректный входной файл");
             }
