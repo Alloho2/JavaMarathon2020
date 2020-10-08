36b8b8a41dbf15f7a691903773b66a82a2e5d1bb
Andrey Baraboshin <alloho2@gmail.com>
2020-10-08 23:27:45 +0300

    Day17 solution update1

--- src/main/java/day17/ChessBoard.java
+++ src/main/java/day17/ChessBoard.java
@@ -4,8 +4,14 @@ public class ChessBoard {
     private ChessPiece[][] chessPiece;
 
     public ChessBoard(ChessPiece[][] chessPiece) {
-
         this.chessPiece = chessPiece;
+        for (int i = 0; i < chessPiece.length; i++) {
+            for (int j = 0; j < chessPiece[0].length; j++) {
+                if (chessPiece[i][j] == null) {
+                    chessPiece[i][j] = ChessPiece.EMPTY;
+                }
+            }
+        }
     }
 
     public void print() {
--- src/main/java/day17/Task2.java
+++ src/main/java/day17/Task2.java
@@ -2,15 +2,31 @@ package day17;
 
 public class Task2 {
     public static void main(String[] args) {
-        ChessPiece[][] chessPiece =
-                {{ChessPiece.ROOK_BLACK, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.ROOK_BLACK, ChessPiece.KING_BLACK, ChessPiece.EMPTY},
-                        {ChessPiece.EMPTY, ChessPiece.ROOK_WHITE, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.PAWN_BLACK, ChessPiece.PAWN_BLACK, ChessPiece.EMPTY, ChessPiece.PAWN_BLACK},
-                        {ChessPiece.PAWN_BLACK, ChessPiece.EMPTY, ChessPiece.KNIGHT_BLACK, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.PAWN_BLACK, ChessPiece.EMPTY},
-                        {ChessPiece.QUEEN__BLACK, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.BISHOP_WHITE, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY},
-                        {ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.BISHOP_BLACK, ChessPiece.PAWN_WHITE, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY},
-                        {ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.BISHOP_WHITE, ChessPiece.PAWN_WHITE, ChessPiece.EMPTY, ChessPiece.EMPTY},
-                        {ChessPiece.PAWN_WHITE, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.QUEEN_WHITE, ChessPiece.EMPTY, ChessPiece.PAWN_WHITE, ChessPiece.EMPTY, ChessPiece.PAWN_WHITE},
-                        {ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.ROOK_WHITE, ChessPiece.KING_WHITE, ChessPiece.EMPTY}};
+
+        ChessPiece[][] chessPiece = new ChessPiece[8][8];
+        chessPiece[0][0] = ChessPiece.ROOK_BLACK;
+        chessPiece[0][5] = ChessPiece.ROOK_BLACK;
+        chessPiece[0][6] = ChessPiece.KING_BLACK;
+        chessPiece[1][1] = ChessPiece.ROOK_WHITE;
+        chessPiece[1][4] = ChessPiece.PAWN_BLACK;
+        chessPiece[1][5] = ChessPiece.PAWN_BLACK;
+        chessPiece[1][7] = ChessPiece.PAWN_BLACK;
+        chessPiece[2][0] = ChessPiece.PAWN_BLACK;
+        chessPiece[2][2] = ChessPiece.KNIGHT_BLACK;
+        chessPiece[2][6] = ChessPiece.PAWN_BLACK;
+        chessPiece[3][0] = ChessPiece.QUEEN__BLACK;
+        chessPiece[3][3] = ChessPiece.BISHOP_WHITE;
+        chessPiece[4][3] = ChessPiece.BISHOP_BLACK;
+        chessPiece[4][4] = ChessPiece.PAWN_WHITE;
+        chessPiece[5][4] = ChessPiece.BISHOP_WHITE;
+        chessPiece[5][5] = ChessPiece.PAWN_WHITE;
+        chessPiece[6][0] = ChessPiece.PAWN_WHITE;
+        chessPiece[6][3] = ChessPiece.QUEEN_WHITE;
+        chessPiece[6][5] = ChessPiece.PAWN_WHITE;
+        chessPiece[6][7] = ChessPiece.PAWN_WHITE;
+        chessPiece[7][5] = ChessPiece.ROOK_WHITE;
+        chessPiece[7][6] = ChessPiece.KING_WHITE;
+
 
         ChessBoard chessBoard = new ChessBoard(chessPiece);
         chessBoard.print();
