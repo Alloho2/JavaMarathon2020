065fe2dfa313d500101e17c30aaad5e60a33923d
Andrey Baraboshin <alloho2@gmail.com>
2020-10-10 00:44:39 +0300

    Day18 solution update

--- src/main/java/day18/Task2.java
+++ src/main/java/day18/Task2.java
@@ -1,19 +1,16 @@
 package day18;
 
 public class Task2 {
-    static int result = 0;
 
     public static void main(String[] args) {
-        System.out.println(count(717771778));
+        System.out.println(count(717771270));
     }
 
     public static int count(int i) {
-
-        if (i < 1) return 0;
+        if (i < 7) return 0;
         if (i % 10 == 7) {
-            result++;
+            return count(i / 10) + 1;
         }
-        count(i / 10);
-        return result;
+        return count(i / 10);
     }
 }
